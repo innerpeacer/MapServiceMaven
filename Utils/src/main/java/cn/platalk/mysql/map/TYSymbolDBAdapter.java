f3322e32c8be911db7e009c8b43f4a8571d75896
innerpeacer <innerpeacer@gmail.com>
2019-06-19 17:35:18 +0800

    Feature: Add IconTextSymbol mysql table adapter

--- Utils/src/main/java/cn/platalk/mysql/map/TYSymbolDBAdapter.java
+++ Utils/src/main/java/cn/platalk/mysql/map/TYSymbolDBAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import cn.platalk.map.entity.base.impl.TYFillSymbolRecord;
 import cn.platalk.map.entity.base.impl.TYIconSymbolRecord;
+import cn.platalk.map.entity.base.impl.TYIconTextSymbolRecord;
 import cn.platalk.sqlhelper.mysql.IPMysqlDB;
 import cn.platalk.sqlhelper.sql.IPSqlTable;
 
@@ -12,13 +13,14 @@ public class TYSymbolDBAdapter {
 	IPMysqlDB db;
 	IPSqlTable fillTable;
 	IPSqlTable iconTable;
+	IPSqlTable iconTextTable;
 
 	public TYSymbolDBAdapter() {
 		db = new IPMysqlDB(TYDatabaseManager.GetMapDBUrl(), TYDatabaseManager.GetUserName(),
 				TYDatabaseManager.GetPassword());
 		fillTable = IPMysqlFillSymbolParams.CreateTable();
 		iconTable = IPMysqlIconSymbolParams.CreateTable();
-
+		iconTextTable = IPMysqlIconTextSymbolParams.CreateTable();
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (InstantiationException e) {
@@ -39,47 +41,24 @@ public class TYSymbolDBAdapter {
 	}
 
 	public void createTableIfNotExist() {
-		// String sql = null;
-		//
-		// sql = "CREATE TABLE IF NOT EXISTS " + TABLE_FILL_SYMBOL + " (_id INT
-		// NOT NULL AUTO_INCREMENT, "
-		// + "SYMBOL_ID INT NOT NULL, " + "FILL VARCHAR(45) NOT NULL, " +
-		// "OUTLINE VARCHAR(45) NOT NULL, "
-		// + "LINE_WIDTH DOUBLE NOT NULL," + "BUILDING_ID VARCHAR(45) NOT NULL,"
-		// + " PRIMARY KEY (_id),"
-		// + " UNIQUE INDEX _id_UNIQUE (_id ASC));";
-		// try {
-		// Statement stmt = connection.createStatement();
-		// stmt.executeUpdate(sql);
-		// } catch (SQLException e) {
-		// e.printStackTrace();
-		// }
-		//
-		// sql = "CREATE TABLE IF NOT EXISTS " + TABLE_ICON_SYMBOL + " (_id INT
-		// NOT NULL AUTO_INCREMENT, "
-		// + "SYMBOL_ID INT NOT NULL, " + "ICON VARCHAR(45) NOT NULL, " +
-		// "BUILDING_ID VARCHAR(45) NOT NULL,"
-		// + " PRIMARY KEY (_id)," + " UNIQUE INDEX _id_UNIQUE (_id ASC));";
-		//
-		// try {
-		// Statement stmt = connection.createStatement();
-		// stmt.executeUpdate(sql);
-		// } catch (SQLException e) {
-		// e.printStackTrace();
-		// }
 		db.createTable(fillTable);
 		db.createTable(iconTable);
+		db.createTable(iconTextTable);
 	}
 
 	public void eraseSymbolTable() {
 		db.eraseTable(fillTable);
 		db.eraseTable(iconTable);
+		db.eraseTable(iconTextTable);
 	}
 
 	public void deleteSymbols(String buildingID) {
 		db.deleteRecord(fillTable, fillTable.getField(IPMysqlFillSymbolParams.FIELD_MAP_SYMBOL_FILL_5_BUILDING_ID),
 				buildingID);
 		db.deleteRecord(iconTable, iconTable.getField(IPMysqlIconSymbolParams.FIELD_MAP_SYMBOL_ICON_3_BUILDING_ID),
+				buildingID);
+		db.deleteRecord(iconTextTable,
+				iconTextTable.getField(IPMysqlIconTextSymbolParams.FIELD_MAP_SYMBOL_ICON_TEXT_100_BUILDING_ID),
 				buildingID);
 	}
 
@@ -99,12 +78,24 @@ public class TYSymbolDBAdapter {
 		return count;
 	}
 
+	public int insertIconTextSymbolRecords(List<TYIconTextSymbolRecord> recordList, String buildingID) {
+		int count = 0;
+		for (TYIconTextSymbolRecord record : recordList) {
+			count += insertIconTextSymbolRecord(record, buildingID);
+		}
+		return count;
+	}
+
 	int insertFillSymbolRecord(TYFillSymbolRecord record, String buildingID) {
 		return db.insertData(fillTable, IPMysqlFillSymbolParams.DataMapFromFillSymbol(record, buildingID));
 	}
 
 	int insertIconSymbolRecord(TYIconSymbolRecord record, String buildingID) {
 		return db.insertData(iconTable, IPMysqlIconSymbolParams.DataMapFromIconSymbol(record, buildingID));
+	}
+
+	int insertIconTextSymbolRecord(TYIconTextSymbolRecord record, String buildingID) {
+		return db.insertData(iconTextTable, IPMysqlIconTextSymbolParams.DataMapFromIconTextSymbol(record, buildingID));
 	}
 
 	public List<TYFillSymbolRecord> getFillSymbolRecords(String buildingID) {
@@ -115,5 +106,11 @@ public class TYSymbolDBAdapter {
 	public List<TYIconSymbolRecord> getIconSymbolRecords(String buildingID) {
 		return IPMysqlIconSymbolParams.IconSymbolListFromRecords(db.readData(iconTable,
 				iconTable.getField(IPMysqlIconSymbolParams.FIELD_MAP_SYMBOL_ICON_3_BUILDING_ID), buildingID));
+	}
+
+	public List<TYIconTextSymbolRecord> getIconTextSymbolRecords(String buildingID) {
+		return IPMysqlIconTextSymbolParams.IconTextSymbolListFromRecords(db.readData(iconTextTable,
+				iconTextTable.getField(IPMysqlIconTextSymbolParams.FIELD_MAP_SYMBOL_ICON_TEXT_100_BUILDING_ID),
+				buildingID));
 	}
 }
