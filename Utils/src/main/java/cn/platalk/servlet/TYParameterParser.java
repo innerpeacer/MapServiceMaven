66255f5f556daea1320a55338d29a3119194cb39
innerpeacer <innerpeacer@gmail.com>
2020-05-20 11:50:07 +0800

    Improvement: Improve Parameter Parser

--- Utils/src/main/java/cn/platalk/servlet/TYParameterParser.java
+++ Utils/src/main/java/cn/platalk/servlet/TYParameterParser.java
@@ -9,84 +9,104 @@ import cn.platalk.map.entity.base.impl.TYLocalPoint;
 
 public class TYParameterParser {
 
-	public static Double getDouble(HttpServletRequest request, String key) {
-		String value = request.getParameter(key);
-		if (value != null) {
-			return Double.parseDouble(value);
-		}
-		return null;
-	}
+    public static Double getDouble(HttpServletRequest request, String key, Double defaultValue) {
+        String value = request.getParameter(key);
+        if (value != null) {
+            return Double.parseDouble(value);
+        }
+        return defaultValue;
+    }
 
-	public static Integer getInteger(HttpServletRequest request, String key) {
-		String value = request.getParameter(key);
-		if (value != null) {
-			return Integer.parseInt(value);
-		}
-		return null;
-	}
+    public static Double getDouble(HttpServletRequest request, String key) {
+        return getDouble(request, key, null);
+    }
 
-	public static String getString(HttpServletRequest request, String key) {
-		return request.getParameter(key);
-	}
+    public static Integer getInteger(HttpServletRequest request, String key, Integer defaultValue) {
+        String value = request.getParameter(key);
+        if (value != null) {
+            return Integer.parseInt(value);
+        }
+        return defaultValue;
+    }
 
-	public static String getString(HttpServletRequest request, String key, String defaultValue) {
-		String value = request.getParameter(key);
-		return value != null ? value : defaultValue;
-	}
+    public static Integer getInteger(HttpServletRequest request, String key) {
+        return getInteger(request, key, null);
+    }
 
-	public static List<String> getStringList(HttpServletRequest request, String key) {
-		List<String> strList = new ArrayList<String>();
-		String str = request.getParameter(key);
-		if (str != null) {
-			String[] strArray = str.split(",");
-			for (int i = 0; i < strArray.length; i++) {
-				strList.add(strArray[i]);
-			}
-		}
-		return strList;
-	}
+    public static Long getLong(HttpServletRequest request, String key, Long defaultValue) {
+        String value = request.getParameter(key);
+        if (value != null) {
+            return Long.parseLong(value);
+        }
+        return defaultValue;
+    }
 
-	public static boolean getBoolean(HttpServletRequest request, String key) {
-		return Boolean.parseBoolean(request.getParameter(key));
-	}
+    public static Long getLong(HttpServletRequest request, String key) {
+        return getLong(request, key);
+    }
 
-	public static boolean getBoolean(HttpServletRequest request, String key, boolean defaultValue) {
-		String value = request.getParameter(key);
-		if (value != null) {
-			return Boolean.parseBoolean(value);
-		}
-		return defaultValue;
-	}
+    public static String getString(HttpServletRequest request, String key) {
+        return getString(request, key, null);
+    }
 
-	public static TYLocalPoint getLocalPoint(HttpServletRequest request, String key) {
-		String str = request.getParameter(key);
-		String[] strArray = str.split(",");
+    public static String getString(HttpServletRequest request, String key, String defaultValue) {
+        String value = request.getParameter(key);
+        return value != null ? value : defaultValue;
+    }
 
-		if (str == null || strArray.length != 3) {
-			return null;
-		}
+    public static List<String> getStringList(HttpServletRequest request, String key) {
+        List<String> strList = new ArrayList<String>();
+        String str = request.getParameter(key);
+        if (str != null) {
+            String[] strArray = str.split(",");
+            for (int i = 0; i < strArray.length; i++) {
+                strList.add(strArray[i]);
+            }
+        }
+        return strList;
+    }
 
-		double x = Double.parseDouble(strArray[0]);
-		double y = Double.parseDouble(strArray[1]);
-		int floor = Integer.parseInt(strArray[2]);
-		return new TYLocalPoint(x, y, floor);
-	}
+    public static Boolean getBoolean(HttpServletRequest request, String key) {
+        return getBoolean(request, key, null);
+    }
 
-	public static List<TYLocalPoint> getLocalPointList(HttpServletRequest request, String key) {
-		List<TYLocalPoint> points = new ArrayList<TYLocalPoint>();
+    public static Boolean getBoolean(HttpServletRequest request, String key, Boolean defaultValue) {
+        String value = request.getParameter(key);
+        if (value != null) {
+            return Boolean.parseBoolean(value);
+        }
+        return defaultValue;
+    }
 
-		String str = request.getParameter(key);
-		if (str != null) {
-			String[] strArray = str.split(",");
-			for (int i = 0; i < strArray.length; i += 3) {
-				double x = Double.parseDouble(strArray[i]);
-				double y = Double.parseDouble(strArray[i + 1]);
-				int floor = Integer.parseInt(strArray[i + 2]);
-				TYLocalPoint p = new TYLocalPoint(x, y, floor);
-				points.add(p);
-			}
-		}
-		return points;
-	}
+    public static TYLocalPoint getLocalPoint(HttpServletRequest request, String key) {
+        String str = request.getParameter(key);
+        String[] strArray = str.split(",");
+
+        if (str == null || strArray.length != 3) {
+            return null;
+        }
+
+        double x = Double.parseDouble(strArray[0]);
+        double y = Double.parseDouble(strArray[1]);
+        int floor = Integer.parseInt(strArray[2]);
+        return new TYLocalPoint(x, y, floor);
+    }
+
+    public static List<TYLocalPoint> getLocalPointList(HttpServletRequest request, String key) {
+        List<TYLocalPoint> points = new ArrayList<TYLocalPoint>();
+
+        String str = request.getParameter(key);
+        if (str != null) {
+            String[] strArray = str.split(",");
+            for (int i = 0; i < strArray.length; i += 3) {
+                double x = Double.parseDouble(strArray[i]);
+                double y = Double.parseDouble(strArray[i + 1]);
+                int floor = Integer.parseInt(strArray[i + 2]);
+                TYLocalPoint p = new TYLocalPoint(x, y, floor);
+                points.add(p);
+            }
+        }
+        return points;
+    }
 
 }
