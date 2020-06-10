e1f43b7d317f1c5354e1a70cb93d96676c5a9544
innerpeacer <innerpeacer@gmail.com>
2020-06-10 11:35:48 +0800

    Feature: Add LocationService Module

---
+++ LocationService/src/main/java/cn/platalk/lbs/api/TYInitServlet.java
@@ -0,0 +1,44 @@
+package cn.platalk.lbs.api;
+
+import cn.platalk.lbs.core.config.TYServerEnvironment;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/initEnv", loadOnStartup = 1)
+public class TYInitServlet extends HttpServlet {
+    private static final long serialVersionUID = -3535588845108542020L;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        StringBuilder builder = new StringBuilder();
+        if (TYServerEnvironment.resourceManager == null) {
+            builder.append("init failed!");
+        } else {
+            builder.append("init success!").append("\n");
+            builder.append("DB Host: ").append(TYServerEnvironment.resourceManager.getDBHost()).append("\n");
+        }
+
+        PrintWriter out = response.getWriter();
+        out.print(builder.toString());
+        out.flush();
+        out.close();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        TYServerEnvironment.initialize();
+    }
+
+}
